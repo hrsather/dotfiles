45df96fce9ef59bc4e45a3daffa5eebba3d6fe5c
Hayden Sather <hsather@am-m16p72k5wc.localdomain>
2025-02-26 07:58:00 -0700

    feat: brew cleanup

--- assets/scripts/update_packages.sh
+++ assets/scripts/update_packages.sh
@@ -3,6 +3,6 @@
 brew update
 brew upgrade
 brew upgrade --cask
-brew bundle --file ~/dotfiles/assets/Brewfile --force
+brew bundle --file ~/dotfiles/assets/Brewfile --force --cleanup
 brew autoremove
 brew cleanup
