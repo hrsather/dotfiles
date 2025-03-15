41f0537a8e1f9f43ae08518bc51256670b1a27e2
buildci <noreply@taservs.net>
2025-03-15 08:23:55 -0600

    feat: Lualine. Headphones

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -1,42 +1,44 @@
 return {
-  'nvim-lualine/lualine.nvim',
-  event = 'VeryLazy',
-  init = function()
-    vim.g.lualine_laststatus = vim.o.laststatus
-    if vim.fn.argc(-1) > 0 then
-      -- set an empty statusline till lualine loads
-      vim.o.statusline = ' '
-    else
-      -- hide the statusline on the starter page
-      vim.o.laststatus = 0
-    end
-  end,
-  opts = function()
-    -- PERF: we don't need this lualine require madness 🤷
-    local lualine_require = require 'lualine_require'
-    lualine_require.require = require
+	"nvim-lualine/lualine.nvim",
+	event = "VeryLazy",
+	init = function()
+		vim.g.lualine_laststatus = vim.o.laststatus
+		if vim.fn.argc(-1) > 0 then
+			-- Set an empty statusline until Lualine loads
+			vim.o.statusline = " "
+		else
+			-- Hide the statusline on the starter page
+			vim.o.laststatus = 0
+		end
+	end,
+	opts = function()
+		-- PERF: Simplify Lualine require
+		local lualine_require = require("lualine_require")
+		lualine_require.require = require
 
-    vim.o.laststatus = vim.g.lualine_laststatus
+		vim.o.laststatus = vim.g.lualine_laststatus
 
-    return {
-      options = {
-        theme = 'auto',
-        globalstatus = true,
-        disabled_filetypes = { statusline = { 'dashboard', 'alpha', 'starter' } },
-      },
-      sections = {
-        lualine_a = { 'mode' },
-        lualine_b = { 'branch' },
-        lualine_y = {
-          { 'progress', separator = ' ', padding = { left = 1, right = 0 } },
-          { 'location', padding = { left = 0, right = 1 } },
-        },
-        lualine_z = {
-          function()
-            return ' ' .. os.date '%R'
-          end,
-        },
-      },
-    }
-  end,
+		return {
+			options = {
+				theme = "auto",
+				globalstatus = true,
+				disabled_filetypes = { statusline = { "dashboard", "alpha", "starter" } },
+			},
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "branch" },
+				lualine_c = { "filename" },
+				lualine_x = {}, -- Removed 'encoding', 'fileformat', 'filetype'
+				lualine_y = {
+					{ "progress", separator = " ", padding = { left = 1, right = 0 } },
+					{ "location", padding = { left = 0, right = 1 } },
+				},
+				lualine_z = {
+					function()
+						return " " .. os.date("%R")
+					end,
+				},
+			},
+		}
+	end,
 }
