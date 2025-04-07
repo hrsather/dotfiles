0e1dd88a136a31a48d937a0a1eb005434b03c1c4
Hayden Sather <hayden@Mac.localdomain>
2025-04-07 10:03:03 -0600

    feat: Remove lsp notify

--- nvim/.config/nvim/lua/plugins/noice.lua
+++ nvim/.config/nvim/lua/plugins/noice.lua
@@ -1,24 +1,42 @@
 return {
-	"folke/noice.nvim",
-	event = "VeryLazy",
-	dependencies = {
-		"MunifTanjim/nui.nvim",
-		"rcarriga/nvim-notify",
-	},
-	opts = function(_, opts)
-		-- Ensure opts is initialized
-		opts = opts or {}
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    dependencies = {
+        "MunifTanjim/nui.nvim",
+        "rcarriga/nvim-notify",
+    },
+    opts = function(_, opts)
+        -- Ensure opts is initialized
+        opts = opts or {}
 
-		-- Apply the notify setup
-		require("notify").setup({ background_colour = "#2E3440" })
+        -- Apply the notify setup
+        require("notify").setup({ background_colour = "#2E3440" })
 
-		-- Merge the lsp override configurations
-		opts.lsp = opts.lsp or {}
-		opts.lsp.override = vim.tbl_deep_extend("force", opts.lsp.override or {}, {
-			["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-			["vim.lsp.util.stylize_markdown"] = true,
-		})
+        -- Merge the lsp override configurations
+        opts.lsp = opts.lsp or {}
+        opts.lsp.override = vim.tbl_deep_extend("force", opts.lsp.override or {}, {
+            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+            ["vim.lsp.util.stylize_markdown"] = true,
+        })
 
-		return opts
-	end,
+        opts.lsp.progress = opts.lsp.progress or {}
+        opts.lsp.progress.enabled = false
+
+        opts.routes = {
+            {
+                filter = { event = "msg_showmode" },
+                view = "notify",
+            },
+            {
+                filter = {
+                    event = "lsp",
+                    kind = "progress",
+                    find = "Diagnosing",
+                },
+                opts = { skip = true },
+            },
+        }
+
+        return opts
+    end,
 }
