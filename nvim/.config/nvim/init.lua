abc31136242858418d8e81e1c6a91c513d94e346
Hayden Sather <hayden@Mac.localdomain>
2025-04-07 09:14:50 -0600

    feat: Custom nord colorscheme

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,62 +1,62 @@
 require("settings")
 require("keymaps")
 
+vim.cmd.colorscheme("nord")
+
 vim.lsp.enable({ "basedpyright", "ruff", "lua", "json", "html", "css", "bash" })
 
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-	if vim.v.shell_error ~= 0 then
-		vim.api.nvim_echo({
-			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-			{ out,                            "WarningMsg" },
-			{ "\nPress any key to exit..." },
-		}, true, {})
-		vim.fn.getchar()
-		os.exit(1)
-	end
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out,                            "WarningMsg" },
+            { "\nPress any key to exit..." },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-	import = "plugins",
+    import = "plugins",
 })
 
-vim.cmd.colorscheme("nord")
-
 vim.api.nvim_create_autocmd('LspAttach', {
-	group = vim.api.nvim_create_augroup('my.lsp', {}),
-	callback = function(args)
-		local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
-		local buf = args.buf
-		local opts = { buffer = buf, silent = true }
+    group = vim.api.nvim_create_augroup('my.lsp', {}),
+    callback = function(args)
+        local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+        local buf = args.buf
+        local opts = { buffer = buf, silent = true }
 
-		-- Keymaps
-		vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-		vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+        -- Keymaps
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
 
-		-- Go to implementation if supported
-		if client:supports_method('textDocument/implementation') then
-			vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-		end
+        -- Go to implementation if supported
+        if client:supports_method('textDocument/implementation') then
+            vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+        end
 
-		-- Enable autocompletion if supported
-		if client and client:supports_method('textDocument/completion') then
-			vim.lsp.completion.enable(true, client.id, buf, { autotrigger = true })
-		end
+        -- Enable autocompletion if supported
+        if client and client:supports_method('textDocument/completion') then
+            vim.lsp.completion.enable(true, client.id, buf, { autotrigger = true })
+        end
 
-		-- Autoformat on save if not already handled by the server
-		if not client:supports_method('textDocument/willSaveWaitUntil')
-		    and client:supports_method('textDocument/formatting') then
-			vim.api.nvim_create_autocmd('BufWritePre', {
-				group = vim.api.nvim_create_augroup('my.lsp.format', { clear = false }),
-				buffer = buf,
-				callback = function()
-					vim.lsp.buf.format({ bufnr = buf, id = client.id, timeout_ms = 1000 })
-				end,
-			})
-		end
-	end,
+        -- Autoformat on save if not already handled by the server
+        if not client:supports_method('textDocument/willSaveWaitUntil')
+            and client:supports_method('textDocument/formatting') then
+            vim.api.nvim_create_autocmd('BufWritePre', {
+                group = vim.api.nvim_create_augroup('my.lsp.format', { clear = false }),
+                buffer = buf,
+                callback = function()
+                    vim.lsp.buf.format({ bufnr = buf, id = client.id, timeout_ms = 1000 })
+                end,
+            })
+        end
+    end,
 })
