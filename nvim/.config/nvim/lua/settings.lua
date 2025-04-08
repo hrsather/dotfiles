4038f3513215ef3b469601b891ddfed24d38750a
Hayden Sather <hsather@am-m16p72k5wc.localdomain>
2025-04-07 18:21:38 -0600

    feat: Don't make .Session if no args

--- nvim/.config/nvim/lua/settings.lua
+++ nvim/.config/nvim/lua/settings.lua
@@ -51,11 +51,14 @@ vim.api.nvim_create_autocmd("VimEnter", {
     pattern = "*",
     nested = true,
     callback = function()
-        local session_file = vim.fn.getcwd() .. '/.Session.vim'
-        if vim.fn.filereadable(session_file) == 1 then
-            vim.cmd('source ' .. session_file)
-        else
-            vim.cmd('mksession! ' .. session_file)
+        -- Check if no file arguments were provided
+        if vim.fn.argc() == 0 then
+            local session_file = vim.fn.getcwd() .. '/.Session.vim'
+            if vim.fn.filereadable(session_file) == 1 then
+                vim.cmd('source ' .. session_file)
+            else
+                vim.cmd('mksession! ' .. session_file)
+            end
         end
     end,
 })
