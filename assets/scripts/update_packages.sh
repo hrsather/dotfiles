0680ae4158a6d413f3e12e93f29793ddb7a900d4
Hayden Sather <hsather@am-m16p72k5wc.localdomain>
2025-04-15 10:52:00 -0600

    feat: Update nvim with update_packages. mpv -> iina

--- assets/scripts/update_packages.sh
+++ assets/scripts/update_packages.sh
@@ -7,3 +7,9 @@ brew bundle --file ~/dotfiles/assets/Brewfile --force --cleanup
 brew cu --all --force
 brew autoremove
 brew cleanup
+
+# Update nvim packages
+nvim --headless "+Lazy! sync" +qa
+
+# Update Treesitter langs
+nvim --headless +TSUpdateSync +qa
