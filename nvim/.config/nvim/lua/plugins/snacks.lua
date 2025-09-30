fbc54d748c86f1e6e7d73bdb580563aa760d45f2
Hayden Sather <hsather@am-m16p72k5wc.localdomain>
2025-09-30 16:07:51 -0600

    fix: picker regex=false

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -11,6 +11,7 @@ return {
                 },
                 grep = {
                     hidden = true,
+                    regex = false,
                 },
             },
             win = {
