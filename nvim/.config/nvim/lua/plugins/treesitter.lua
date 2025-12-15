601311fbb6b8dd8a3fe1d8c10143f30696195774
Hayden Sather <hayden@Mac.localdomain>
2025-12-14 23:26:41 -0700

    fix: Downgrade Treesitter. Fix Docker parser

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -2,14 +2,12 @@ return {
     {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
+        -- TODO: Update to "main" branch once 0.12 is out
+        branch = "master",
         main = 'nvim-treesitter.configs',
         opts = {
-            ensure_installed = { 'python', 'yaml', 'json', 'bash', 'html', 'lua', 'markdown', 'markdown_inline', 'Dockerfile' },
-            -- Autoinstall languages that are not installed
-            auto_install = true,
-            highlight = {
-                enable = true,
-            },
+            ensure_installed = { 'python', 'yaml', 'json', 'bash', 'html', 'toml', 'lua', 'markdown', 'markdown_inline', 'dockerfile', 'jq' },
+            highlight = { enable = true },
             indent = { enable = true },
             incremental_selection = {
                 enable = true,
